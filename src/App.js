e55527a586a5db436a4c8ae568e93cb669a7a89b
Fabian VC <fabianvera728@gmail.com>
2021-12-22 12:14:35 -0500

    [add] login is running

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 /*global chrome*/
-
 import React from "react";
 import "./App.css";
 import { useForm } from "react-hook-form";
 
 const App = ({ docframe }) => {
   // const doc = window.frames[1].document;
+  const search = () => {
+    const itemsCount = 37;
+    let dicc = [];
+    for (let i = 0; i < itemsCount; i++) {
+      const textInput = docframe.getElementsByClassName(
+        "fondo_celda_5 text_letras"
+      )[i].innerHTML;
+      const numberImput = docframe.getElementsByClassName(
+        "fondo_celda_3 text_num"
+      )[i].innerHTML;
+      dicc[textInput] = numberImput;
+    }
+    return dicc;
+  };
+
+  const decode = (password) => {
+    var dicc = search();
+    var passwordSplit = password.split("");
+    var decodedPassword = [];
+    for (var i = 0; i < passwordSplit.length; i++) {
+      var nombre = passwordSplit[i].toUpperCase();
+      decodedPassword[i] = dicc[nombre];
+    }
+    return decodedPassword.join("");
+  };
+
   const parentLoginButton = docframe.querySelector('[name="btnIngresar"]');
-  const parentUsernameInput = docframe.querySelector("#usuario")
-  const parentPasswordInput = docframe.querySelector("#password")
+  const parentUsernameInput = docframe.querySelector("#usuario");
+  const parentPasswordInput = docframe.querySelector("#password");
   const {
     register,
     handleSubmit,
@@ -18,9 +43,10 @@ const App = ({ docframe }) => {
   const onSubmit = (data) => {
     console.log(data);
     parentUsernameInput.value = data.username;
-    parentPasswordInput.value = data.password;
+    parentPasswordInput.value = decode(data.password);
+    parentLoginButton.click();
   };
-  // console.log(window.frames[1].document.querySelector('[name="btnIngresar"]'));
+
   return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
