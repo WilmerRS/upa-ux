da4eb56f5b9e28ff495d9062dea3707244add616
Fabian VC <fabianvera728@gmail.com>
2021-12-22 10:34:07 -0500

    [add] form

--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useForm } from "react-hook-form";
 
 const App = ({ docframe, isExt }) => {
   // const doc = window.frames[1].document;
   const a = docframe.querySelector('[name="btnIngresar"]');
   a.value = "Loggin";
   console.log({ a });
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   // console.log(window.frames[1].document.querySelector('[name="btnIngresar"]'));
   return (
     <div className="App">
-      <header className="App-header">
-        {isExt ? (
-          <img
-            src={chrome.runtime.getURL("static/media/logo.svg")}
-            className="App-logo"
-            alt="logo"
-          />
-        ) : (
-          <img src={logo} className="App-logo" alt="logo" />
-        )}
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="username" {...register("username")} />
+        <input name="password" {...register("password")} />
+        <input type="submit" value="Send form" />
+      </form>
     </div>
   );
 };
