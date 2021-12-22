6d5c302eb61ce38d1fd5ad2eef52fde479a7b95b
Fabian VC <fabianvera728@gmail.com>
2021-12-22 11:23:20 -0500

    [add] create form login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 /*global chrome*/
 
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import { useForm } from "react-hook-form";
 
 const App = ({ docframe, isExt }) => {
   // const doc = window.frames[1].document;
-  const a = docframe.querySelector('[name="btnIngresar"]');
-  a.value = "Loggin";
-  console.log({ a });
+  const parentLoginButton = docframe.querySelector('[name="btnIngresar"]');
+  const parentUsernameInput = docframe.querySelector("#usuario")
+  const parentPasswordInput = docframe.querySelector("#password")
   const {
     register,
     handleSubmit,
@@ -18,13 +17,15 @@ const App = ({ docframe, isExt }) => {
   } = useForm();
   const onSubmit = (data) => {
     console.log(data);
+    parentUsernameInput.value = data.username;
+    parentPasswordInput.value = data.password;
   };
   // console.log(window.frames[1].document.querySelector('[name="btnIngresar"]'));
   return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <input name="username" {...register("username")} />
-        <input name="password" {...register("password")} />
+        <input type="password" name="password" {...register("password")} />
         <input type="submit" value="Send form" />
       </form>
     </div>
