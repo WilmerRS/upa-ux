835009d9e05040bffc503c889755828fc43559c9
WilmerRS <estwilmerrodriguez@gmail.com>
2022-10-31 13:48:54 -0500

    root:🌀: build context as separate bundle

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        home: "src/contexts/home/index.ts",
+        authentication: "src/contexts/authentication/index.ts",
+      },
+      output:{
+        entryFileNames: 'assets/[name].js',
+        assetFileNames:'assets/[name].[ext]',
+        dir:'dist'
+      }
+    },
+  },
+});
