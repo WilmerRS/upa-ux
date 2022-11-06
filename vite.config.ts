80fbd300c2016444c864d5bf741b719074c4e4b4
WilmerRS <estwilmerrodriguez@gmail.com>
2022-11-06 14:47:17 -0500

    root:🌀: add tailwind styles

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,11 @@ export default defineConfig({
         home: "src/contexts/home/index.ts",
         authentication: "src/contexts/authentication/index.ts",
       },
-      output:{
-        entryFileNames: 'assets/[name].js',
-        assetFileNames:'assets/[name].[ext]',
-        dir:'dist'
-      }
+      output: {
+        entryFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+        dir: "dist",
+      },
     },
   },
 });
