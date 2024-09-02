9ffc6ed0728d0fcce4d2222b1bde554ab1751d00
wilmerrs <est.wilmerrordriguez@gmail.com>
2024-09-01 18:43:36 -0600

    feat: update login page styles with Tailwind CSS

--- src/contexts/authentication/presentation/App.tsx
+++ src/contexts/authentication/presentation/App.tsx
@@ -22,44 +22,63 @@ const App = ({ documentFrame }: { documentFrame: Document }) => {
 
   return (
     <div className="grid place-content-center h-screen">
-      <div>
-        <h1 className="text-center font-black text-3xl underline mb-12 text-violet-500">
-          UPA UX!
+      <div className="w-full sm:w-[350px]">
+        <h1 className="text-center font-black text-3xl mb-12 text-gray-900">
+          Campus Virtual
         </h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="">
+
+        <form onSubmit={handleSubmit(onSubmit)}>
           <section className="mb-10 flex flex-col text-slate-700">
-            <div className="mb-3">
+            <div className="mb-2">
               <label
-                className="text-sm font-semibold mb-1 block"
+                className="text-sm text-gray-600 mb-1 block"
                 htmlFor="username"
               >
                 Usuario
               </label>
               <input
                 id="username"
-                className="py-2 px-3 block rounded-md border border-slate-100 transition-all duration-300 transform hover:border-violet-400 focus:outline-none focus:ring focus:ring-violet-300 focus:ring-opacity-80"
-                {...register("username")}
+                className="w-full py-3 px-3 block rounded-md border border-slate-100 transition-all duration-300 transform hover:border-gray-800 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-80"
+                {...register("username", { required: true, minLength: 3 })}
+                aria-invalid={errors.username ? "true" : "false"}
               />
+              <p className="text-red-600 min-h-6">
+                {errors.username?.type === "required" &&
+                  "Este campo es requerido"}
+                {errors.username?.type === "minLength" &&
+                  "Este campo debe tener al menos 3 caracteres"}
+                {!errors.username && ""}
+              </p>
             </div>
-            <div className="mb-3">
+
+            <div className="mb-2">
               <label
-                className="text-sm font-semibold mb-1 block"
+                className="text-sm text-gray-600 mb-1 block"
                 htmlFor="password"
               >
                 Contraseña
               </label>
               <input
                 id="password"
-                className="py-2 px-3 block rounded-md border border-slate-100 transition-all duration-300 transform hover:border-violet-400 focus:outline-none focus:ring focus:ring-violet-300 focus:ring-opacity-80"
+                className="w-full py-3 px-3 block rounded-md border border-slate-100 transition-all duration-300 transform hover:border-gray-800 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-80"
                 type="password"
-                {...register("password")}
+                {...register("password", { required: true, minLength: 8 })}
+                aria-invalid={errors.password ? "true" : "false"}
               />
+              <p className="text-red-600 min-h-6">
+                {errors.password?.type === "required" &&
+                  "Este campo es requerido"}
+                {errors.password?.type === "minLength" &&
+                  "Este campo debe tener al menos 8 caracteres"}
+                {!errors.password && ""}
+              </p>
             </div>
           </section>
+
           <div className="flex justify-center">
             <button
               type="submit"
-              className=" px-6 py-2 font-medium tracking-wide text-white capitalize transition-all duration-300 transform bg-violet-500 rounded-md hover:bg-violet-400 focus:outline-none focus:ring focus:ring-violet-300 focus:ring-opacity-80"
+              className="w-full px-6 py-3 font-medium tracking-wide text-white capitalize transition-all duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-80"
             >
               Enviar
             </button>
