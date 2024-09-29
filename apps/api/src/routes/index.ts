11ad9ebe6af4fe24753d522009f19b79e4404be4
wilmerrs <est.wilmerrordriguez@gmail.com>
2024-09-29 15:44:36 -0500

    fix(api): register routes

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,33 +1,9 @@
 import { FastifyInstance } from "fastify";
 
+import { register as appRoutesRegister } from "./app.route";
+import { register as healthRoutesRegister } from "./health.route";
+
 export function registerRoutes(app: FastifyInstance) {
-  //   const routes = globSync(__dirname + "/**/*.route.*");
-  //   routes
-  //     .map((routePath) => {
-  //       const fileNameRegex = /[\/\\]([^\/\\]+)\.route(?=\.ts$)/;
-  //       const fileName = routePath.match(fileNameRegex);
-  //       if (!fileName) {
-  //         app.log.error(` - [ ] Skipping route: ${routePath}`);
-  //         return null;
-  //       }
-
-  //       const route = fileName[1];
-  //       return `./${route}.route`;
-  //     })
-  //     .filter((routePath) => routePath !== null)
-  //     .forEach((routePath) => register(app, routePath));
-  const routes = ["app", "health"];
-
-  routes
-    .map((routePath) => {
-      return `./${routePath}.route`;
-    })
-    .filter((routePath) => routePath !== null)
-    .forEach((routePath) => register(app, routePath));
-}
-
-function register(app: FastifyInstance, routePath: string) {
-  app.log.info(` - [x] Registering route: ${routePath}`);
-  const route = require(routePath);
-  route.register(app);
+  const routes = [appRoutesRegister, healthRoutesRegister];
+  routes.forEach((register) => register(app));
 }
