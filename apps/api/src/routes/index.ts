bf804827f4686128eea082002fc6c601c63fa292
wilmerrs <est.wilmerrordriguez@gmail.com>
2024-09-29 13:45:20 -0500

    fix(api): register routes

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -5,13 +5,21 @@ export function registerRoutes(app: FastifyInstance) {
   const routes = globSync(__dirname + "/**/*.route.*");
   routes
     .map((routePath) => {
-      const route = routePath.replace(/\.ts$/, "").split("routes")[1];
-      return `.${route}`;
+      const fileNameRegex = /[\/\\]([^\/\\]+)\.route(?=\.ts$)/;
+      const fileName = routePath.match(fileNameRegex);
+      if (!fileName) {
+        return null;
+      }
+
+      const route = fileName[1];
+      return `./${route}.route`;
     })
+    .filter((routePath) => routePath !== null)
     .forEach((routePath) => register(app, routePath));
 }
 
 function register(app: FastifyInstance, routePath: string) {
+  app.log.info(` - [x] Registering route: ${routePath}`);
   const route = require(routePath);
   route.register(app);
 }
